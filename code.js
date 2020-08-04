de618f29e331222f82a5aff542c336c2e0637ddd
Галактион <visulina@ya.ru>
2020-08-04 20:25:32 +0300

    hi

--- code.js
+++ code.js
@@ -1,10 +1,29 @@
-const card = document.querySelector('.card');
-const flex_center = document.querySelector('.flex_center');
-const flex_between = document.querySelector('.flex_between');
-
-flex_center.onclick = () =>{
+let card = document.querySelector('.card');
+let flex_center = document.querySelector('.flex_center');
+let flex_between = document.querySelector('.flex_between');
+let flex_start = document.querySelector('.flex_start');
+let flex_end = document.querySelector('.flex_end');
+let space_around = document.querySelector('.space_around');
+flex_center.onclick = () => {
+    // alert("нажатие")
     card.style.justifyContent = "center";
 }
-flex_between.onclick = () =>{
+
+flex_between.onclick = () => {
+    // alert("нажатие")
     card.style.justifyContent = "space-between";
+}
+
+flex_start.onclick = () => {
+    // alert("нажатие")
+    card.style.justifyContent = "flex-start";
+}
+
+flex_end.onclick = () => {
+    // alert("нажатие")
+    card.style.justifyContent = "flex-end";
+}
+space_around.onclick = () => {
+    // alert("нажатие")
+    card.style.justifyContent = "space-around";
 }
